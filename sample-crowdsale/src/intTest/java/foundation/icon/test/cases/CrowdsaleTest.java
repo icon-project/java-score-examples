ccb6c52243dda8f9d6812894ab8f4b40d5ab9d59
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-09-08 17:56:16 +0900

    Add integration tests for crowdsale

---
+++ sample-crowdsale/src/intTest/java/foundation/icon/test/cases/CrowdsaleTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2019 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package foundation.icon.test.cases;
+
+import foundation.icon.icx.IconService;
+import foundation.icon.icx.KeyWallet;
+import foundation.icon.icx.data.Bytes;
+import foundation.icon.icx.data.TransactionResult;
+import foundation.icon.icx.transport.http.HttpProvider;
+import foundation.icon.test.Constants;
+import foundation.icon.test.Env;
+import foundation.icon.test.TestBase;
+import foundation.icon.test.TransactionHandler;
+import foundation.icon.test.score.CrowdSaleScore;
+import foundation.icon.test.score.SampleTokenScore;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static foundation.icon.test.Env.LOG;
+
+class CrowdsaleTest extends TestBase {
+    private static TransactionHandler txHandler;
+    private static KeyWallet ownerWallet;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        Env.Chain chain = Env.getDefaultChain();
+        IconService iconService = new IconService(new HttpProvider(chain.getEndpointURL(3)));
+        txHandler = new TransactionHandler(iconService, chain);
+        ownerWallet = KeyWallet.create();
+    }
+
+    @Test
+    void deployAndStartCrowdsale() throws Exception {
+        // deploy token SCORE
+        BigInteger decimals = BigInteger.valueOf(18);
+        BigInteger initialSupply = BigInteger.valueOf(1000);
+        SampleTokenScore tokenScore = SampleTokenScore.mustDeploy(txHandler, ownerWallet,
+                decimals, initialSupply);
+
+        // deploy crowdsale SCORE
+        BigInteger fundingGoalInIcx = BigInteger.valueOf(100);
+        CrowdSaleScore crowdsaleScore = CrowdSaleScore.mustDeploy(txHandler, ownerWallet,
+                tokenScore.getAddress(), fundingGoalInIcx);
+
+        startCrowdsale(tokenScore, crowdsaleScore, initialSupply, fundingGoalInIcx);
+    }
+
+    void startCrowdsale(SampleTokenScore tokenScore, CrowdSaleScore crowdsaleScore,
+                        BigInteger initialSupply, BigInteger fundingGoalInIcx) throws Exception {
+        KeyWallet aliceWallet = KeyWallet.create();
+        KeyWallet bobWallet = KeyWallet.create();
+
+        // send 50 icx to Alice, 100 to Bob
+        LOG.infoEntering("transfer icx", "50 to Alice; 100 to Bob");
+        transferAndCheckResult(txHandler, aliceWallet.getAddress(), ICX.multiply(BigInteger.valueOf(50)));
+        transferAndCheckResult(txHandler, bobWallet.getAddress(), ICX.multiply(BigInteger.valueOf(100)));
+        LOG.infoExiting();
+
+        // transfer all tokens to crowdsale score
+        LOG.infoEntering("transfer token", "all tokens to crowdsale score from owner");
+        TransactionResult result = tokenScore.transfer(ownerWallet, crowdsaleScore.getAddress(), ICX.multiply(initialSupply));
+        crowdsaleScore.ensureFundingGoal(result, fundingGoalInIcx);
+        tokenScore.ensureTokenBalance(crowdsaleScore.getAddress(), initialSupply.longValue());
+        LOG.infoExiting();
+
+        // send icx to crowdsale score from Alice and Bob
+        LOG.infoEntering("transfer icx", "to crowdsale score (40 from Alice, 60 from Bob)");
+        Bytes[] ids = new Bytes[2];
+        ids[0] = txHandler.transfer(aliceWallet, crowdsaleScore.getAddress(), ICX.multiply(BigInteger.valueOf(40)));
+        ids[1] = txHandler.transfer(bobWallet, crowdsaleScore.getAddress(), ICX.multiply(BigInteger.valueOf(60)));
+        for (Bytes id : ids) {
+            assertSuccess(txHandler.getResult(id));
+        }
+        tokenScore.ensureTokenBalance(aliceWallet.getAddress(), 40);
+        tokenScore.ensureTokenBalance(bobWallet.getAddress(), 60);
+        LOG.infoExiting();
+
+        // check if goal reached
+        LOG.infoEntering("call", "checkGoalReached()");
+        crowdsaleScore.ensureCheckGoalReached(ownerWallet);
+        LOG.infoExiting();
+
+        // do safe withdrawal
+        LOG.infoEntering("call", "safeWithdrawal()");
+        BigInteger oldBal = txHandler.getBalance(ownerWallet.getAddress());
+        result = crowdsaleScore.safeWithdrawal(ownerWallet);
+        if (!Constants.STATUS_SUCCESS.equals(result.getStatus())) {
+            throw new IOException("Failed to execute safeWithdrawal.");
+        }
+        BigInteger amount = ICX.multiply(BigInteger.valueOf(100));
+        crowdsaleScore.ensureFundTransfer(result, ownerWallet.getAddress(), amount);
+
+        // check the final icx balance of owner
+        LOG.info("ICX balance before safeWithdrawal: " + oldBal);
+        BigInteger fee = result.getStepUsed().multiply(result.getStepPrice());
+        BigInteger newBal = oldBal.add(amount).subtract(fee);
+        ensureIcxBalance(txHandler, ownerWallet.getAddress(), oldBal, newBal);
+        LOG.infoExiting();
+    }
+}
---
+++ sample-crowdsale/src/intTest/java/foundation/icon/test/score/CrowdSaleScore.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2018 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package foundation.icon.test.score;
+
+import foundation.icon.icx.Wallet;
+import foundation.icon.icx.data.Address;
+import foundation.icon.icx.data.IconAmount;
+import foundation.icon.icx.data.TransactionResult;
+import foundation.icon.icx.transport.jsonrpc.RpcObject;
+import foundation.icon.icx.transport.jsonrpc.RpcValue;
+import foundation.icon.test.Constants;
+import foundation.icon.test.ResultTimeoutException;
+import foundation.icon.test.TransactionFailureException;
+import foundation.icon.test.TransactionHandler;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static foundation.icon.test.Env.LOG;
+
+public class CrowdSaleScore extends Score {
+
+    public static CrowdSaleScore mustDeploy(TransactionHandler txHandler, Wallet wallet,
+                                            Address tokenAddress, BigInteger fundingGoalInIcx)
+            throws ResultTimeoutException, TransactionFailureException, IOException {
+        LOG.infoEntering("deploy", "Crowdsale");
+        RpcObject params = new RpcObject.Builder()
+                .put("_fundingGoalInIcx", new RpcValue(fundingGoalInIcx))
+                .put("_tokenScore", new RpcValue(tokenAddress))
+                .put("_durationInBlocks", new RpcValue(BigInteger.valueOf(10)))
+                .build();
+        Score score = txHandler.deploy(wallet, getFilePath("sample-crowdsale"), params);
+        LOG.info("scoreAddr = " + score.getAddress());
+        LOG.infoExiting();
+        return new CrowdSaleScore(score);
+    }
+
+    public CrowdSaleScore(Score other) {
+        super(other);
+    }
+
+    public TransactionResult checkGoalReached(Wallet wallet)
+            throws ResultTimeoutException, IOException {
+        return invokeAndWaitResult(wallet, "checkGoalReached", null, null, Constants.DEFAULT_STEPS);
+    }
+
+    public TransactionResult safeWithdrawal(Wallet wallet)
+            throws ResultTimeoutException, IOException {
+        return invokeAndWaitResult(wallet, "safeWithdrawal", null, null, Constants.DEFAULT_STEPS);
+    }
+
+    public void ensureCheckGoalReached(Wallet wallet) throws Exception {
+        while (true) {
+            TransactionResult result = checkGoalReached(wallet);
+            if (!Constants.STATUS_SUCCESS.equals(result.getStatus())) {
+                throw new IOException("Failed to execute checkGoalReached.");
+            }
+            TransactionResult.EventLog event = findEventLog(result, getAddress(), "GoalReached(Address,int)");
+            if (event != null) {
+                break;
+            }
+            LOG.info("Sleep 1 second.");
+            Thread.sleep(1000);
+        }
+    }
+
+    public void ensureFundingGoal(TransactionResult result, BigInteger fundingGoalInIcx)
+            throws IOException {
+        TransactionResult.EventLog event = findEventLog(result, getAddress(), "CrowdsaleStarted(int,int)");
+        if (event != null) {
+            BigInteger fundingGoalInLoop = IconAmount.of(fundingGoalInIcx, IconAmount.Unit.ICX).toLoop();
+            BigInteger fundingGoalFromScore = event.getData().get(0).asInteger();
+            if (fundingGoalInLoop.equals(fundingGoalFromScore)) {
+                return; // ensured
+            }
+        }
+        throw new IOException("ensureFundingGoal failed.");
+    }
+
+    public void ensureFundTransfer(TransactionResult result, Address backer, BigInteger amount)
+            throws IOException {
+        TransactionResult.EventLog event = findEventLog(result, getAddress(), "FundTransfer(Address,int,bool)");
+        if (event != null) {
+            Address _backer = event.getIndexed().get(1).asAddress();
+            BigInteger _amount = event.getIndexed().get(2).asInteger();
+            Boolean isContribution = event.getIndexed().get(3).asBoolean();
+            if (backer.equals(_backer) && amount.equals(_amount) && !isContribution) {
+                return; // ensured
+            }
+        }
+        throw new IOException("ensureFundTransfer failed.");
+    }
+}
